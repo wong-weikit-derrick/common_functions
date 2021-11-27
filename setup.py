18ebc1c9369cc70f98d24f5e68492e83647c95f7
Derrick Wong <darkwing2025@gmail.com>
2021-11-27 13:37:54 +0800

    'initialcommit'

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content if 'git+' not in x]
+
+setup(name='common_functions',
+      version="1.0",
+      description="Project Description",
+      packages=find_packages(),
+      install_requires=requirements,
+      test_suite='tests',
+      # include_package_data: to install data from MANIFEST.in
+      include_package_data=True,
+      scripts=['scripts/common_functions-run'],
+      zip_safe=False)
